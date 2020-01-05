8969b459530d56dd0545514f0c1aa1ead16d4f88
Clicia Scarlet <yuyuvn@mac.com>
2020-01-05 17:06:06 +0900

    Support docker & enable learn app

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const PORT = process.env.PORT || 1880;
+const LEARN_PORT = 1881;
 
 const BroadlinkServer = require('broadlink-rm-server');
 const commands = require('./commands');
 
 // /learn/command/MAC_OR_IP
-// let app = BroadlinkServer(commands, true);
-let app = BroadlinkServer(commands);
+const learnApp = BroadlinkServer(commands, true);
+const app = BroadlinkServer(commands);
 app.listen(PORT);
+learnApp.listen(LEARN_PORT);
 
 console.log('Server running, go to http://localhost:' + PORT);
